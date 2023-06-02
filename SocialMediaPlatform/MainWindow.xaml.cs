67adcb7824a12daa08a29703959bb1724ea49c0a
Hlib <gleb.brodskiy.04@gmail.com>
2023-06-02 10:47:44 +0300

    deleted comments

--- SocialMediaPlatform/MainWindow.xaml.cs
+++ SocialMediaPlatform/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace SocialMediaPlatform
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Button clickedButton = (Button)sender;
+            Button clickedButton = (Button)sender; 
             RegistrateOrLogin registrateOrLogin = new RegistrateOrLogin();
             this.Close();
             registrateOrLogin.Show();
@@ -37,75 +37,6 @@ namespace SocialMediaPlatform
             {
                 registrateOrLogin.Button.Content = "Sign up";
             }
-
-            /*            if (String.IsNullOrEmpty(LoginTextBox.Text) || String.IsNullOrWhiteSpace(LoginTextBox.Text))
-                        {
-                            MessageBox.Show("You haven't written a login. Please try again.", "Login is no written.");
-                            //throw new ApplicationException("Customer name is mandatory.");
-                            return;
-                        }
-                        if (String.IsNullOrEmpty(PasswordTextBox.Text) || String.IsNullOrWhiteSpace(PasswordTextBox.Text))
-                        {
-                            MessageBox.Show("You haven't written a password. Please try again.", "Password is no written.");
-                            //throw new ApplicationException("Customer name is mandatory.");
-                            return;
-                        }
-                        Login =  LoginTextBox.Text;
-                        Password = PasswordTextBox.Text;
-
-                        if (Login.Any(Char.IsWhiteSpace))
-                        {
-                            Login = Login.Replace(" ", "");
-                        }
-                        if (Password.Any(Char.IsWhiteSpace))
-                        {
-                            Password = Password.Replace(" ", "");
-                        }
-
-                        MessageBox.Show("You created and account.", "Account created.");
-
-
-                        RegistrateButton.Name = "Login";
-
-                        if (String.IsNullOrEmpty(LoginTextBox.Text) || String.IsNullOrWhiteSpace(LoginTextBox.Text))
-                        {
-                            MessageBox.Show("You haven't written a login. Please try again.", "Login is no written.");
-                            //throw new ApplicationException("Customer name is mandatory.");
-                            return;
-                        }
-                        if (String.IsNullOrEmpty(PasswordTextBox.Text) || String.IsNullOrWhiteSpace(PasswordTextBox.Text))
-                        {
-                            MessageBox.Show("You haven't written a password. Please try again.", "Password is no written.");
-                            //throw new ApplicationException("Customer name is mandatory.");
-                            return;
-                        }
-
-                        if (LoginTextBox.Text == Login)
-                        {
-                            MessageBox.Show("You have logged in", "Login in success.");
-
-                            return;
-                        }
-                        if (PasswordTextBox.Text == Password)
-                        {
-                            MessageBox.Show("You have logged in", "Login in success.");
-                            return;
-                        }
-
-            */
-
-            /*
-             Registrate"
-             LoginTextBox
-             Login
-             PasswordText
-             Password
-             
-             
-             */
-
-            /*            MainWindow mainWindow = new MainWindow();
-                        mainWindow.Show();*/
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
